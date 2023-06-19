f87cbea25a9ee9c98274a30d26a4cbabab027743
liskunovich <david.leonov.96@mail.ru>
2023-06-19 14:24:55 +0600

    Added bash script for parallel apps running

--- manage.py
+++ manage.py
@@ -1,9 +1,13 @@
+import os
+
 from flask_app import create_app, db
 app = create_app()
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    host = os.environ.get('FLASK_IP', '0.0.0.0')
+    port = os.environ.get('FLASK_PORT', 8000)
+    app.run(host=host, port=port)
 
 
 
